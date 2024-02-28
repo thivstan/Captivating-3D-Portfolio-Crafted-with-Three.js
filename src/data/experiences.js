3baa3c63f63e999e615971fef2f1fa14284b84f9
Thivstan James <114639488+thivistan@users.noreply.github.com>
2024-02-27 18:15:28 -0800

    Update experiences.js

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -12,7 +12,7 @@ export const experiences = [
     joined: "Aug'23",
     end: "Jan'23",
     title: "Software Developer",
-    bio:"In my role as a Software Developer from August 2023 to January 2024, I specialized in crafting responsive single-page applications for non-profit organizations. Leveraging technologies such as Java, Spring Boot, Angular, and TypeScript, I enhanced UI responsiveness by 70%, optimized portal throughput by 85%, and elevated code quality by 45% through automation and meticulous testing. Additionally, I spearheaded the adoption of GraphQL, resulting in a 30% surge in API efficiency, and streamlined development processes, reducing cycle times from 12 weeks to just 7 days.",
+    bio:"In my role as a Software Developer from Sep 2023 to Dec 2023,  proficient in Angular, TypeScript, and HTML/CSS. Led teams to improve UI responsiveness by 70% and drive a 25% increase in user satisfaction through feature development. Implemented advanced rendering techniques, reducing rendering time by 40% and improving project delivery speed by 25%. Automated 60% of infrastructure tasks, boosting code quality by 45%. Spearheaded GraphQL adoption, resulting in a 30% API efficiency surge. Conducted bi-weekly code reviews, achieving 95% code coverage and reducing critical issues by 30%. Passionate about delivering innovative solutions that optimize performance and elevate user experience.",
     color: blue,
   },
   {
@@ -21,7 +21,7 @@ export const experiences = [
     joined: "Jan'22",
     end: "Aug'22",
     title: "Software Engineer",
-    bio: "As a Software Engineer from January 2021 to August 2022, I drove transformative initiatives at Optus Network Service. My expertise in Java, Spring Boot, and microservices enabled the development of fault-tolerant back-end systems, ensuring seamless operations during peak traffic. On the front end, I revamped the user interface using JavaScript, jQuery, JSON, and ReactJS, resulting in improved retention and conversion rates.",
+    bio: "As a Software Engineer from January 2021 to August 2022,  Focused on frontend and backend development, specializing in JavaScript, ReactJS, and Spring Boot. Notable achievements include enhancing user retention by 40% through frontend enhancements and improving system performance by 30% via advanced Redux state management techniques. Proficient in Java microservices development, achieving exceptional fault tolerance rates of 99.8%. Demonstrated ability to resolve urgent production issues swiftly, ensuring 100% client satisfaction. Leveraged Spring Cloud to implement secure authentication and enhance system responsiveness by 30%. Led Agile teams to deliver over 10 applications on AWS, utilizing Docker, Kubernetes, and Jenkins for 75% faster deployments and a 35% productivity gain.",
     //bio: "I excelled in problem-solving, achieving 100% customer satisfaction by swiftly resolving production issues using SQL and Stored Procedures. Leading an Agile team, I orchestrated the delivery of numerous applications on AWS, accelerating deployment and enhancing productivity by 35%. Implementing Spring Cloud and optimizing algorithms, I significantly improved system responsiveness while reducing memory usage by 65%.",
     color: green,
   },
